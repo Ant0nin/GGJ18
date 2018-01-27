2b22ef123a72a7066e4e9e0f3d6267a8d4771201
Antonin Bernardin <antonin.bernardin@gmail.com>
2018-01-27 02:37:47 +0100

    spot orientation

---
+++ Assets/Controllers/SpotController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotController : MonoBehaviour {
+
+    Camera cam;
+
+	void Start () {
+        cam = Camera.allCameras[0];
+	}
+	
+    private void OnMouseDrag()
+    {
+        Vector2 viewportMousePos = cam.ScreenToViewportPoint(Input.mousePosition);
+        Vector2 cursorPos = 0.5f * viewportMousePos + new Vector2(0.5f, 0.5f); // [-1;1]
+        Vector2 spotPos = cam.WorldToViewportPoint(transform.position);
+        Vector2 dir = Vector3.Normalize(cursorPos - spotPos);
+        Quaternion orient = Quaternion.LookRotation(dir);
+        transform.rotation = orient;
+    }
+}
