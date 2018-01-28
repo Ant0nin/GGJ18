2266b7495378289403aacab53be708160e22b848
Antonin Bernardin <antonin.bernardin@gmail.com>
2018-01-28 08:38:15 +0100

    door and its related button

---
+++ Assets/ButtonBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour {
+
+    public DoorBehavior targetDoor;
+    public float timeBeforeCloseAction = 2f;
+
+    GameObject buttonOff;
+    GameObject buttonOn;
+    float timeCounterOn = 0f;
+    bool state;
+
+	void Start () {
+        if (targetDoor == null)
+            targetDoor = GameObject.Find("Door").GetComponent<DoorBehavior>();
+
+        buttonOff = transform.GetChild(0).gameObject;
+        buttonOn = transform.GetChild(1).gameObject;
+	}
+
+    private void Update()
+    {
+        if(state == true) // on
+            timeCounterOn += Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        if(timeCounterOn > timeBeforeCloseAction)
+            SwitchOff();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // TODO: trigger sound
+        SwitchOn();
+
+        StartCoroutine(OpenDoorThenClose());
+    }
+
+    IEnumerator OpenDoorThenClose()
+    {
+        yield return new WaitForSeconds(timeBeforeCloseAction);
+        if (state == true) // on
+            SwitchOff();
+    }
+
+    void SwitchOn()
+    {
+        targetDoor.OpenDoor();
+        state = true; // on
+        buttonOn.SetActive(true);
+        buttonOff.SetActive(false);
+    }
+
+    void SwitchOff()
+    {
+        targetDoor.CloseDoor();
+        state = false; // off
+        buttonOff.SetActive(true);
+        buttonOn.SetActive(false);
+        timeCounterOn = 0f;
+    }
+}
---
+++ Assets/DoorBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour {
+
+    GameObject doorClosed;
+    GameObject doorOpen;
+    BoxCollider2D boxCollider;
+
+    void Start () {
+        doorClosed = transform.GetChild(0).gameObject;
+        doorOpen = transform.GetChild(1).gameObject;
+        boxCollider = GetComponent<BoxCollider2D>();
+	}
+
+    public void OpenDoor()
+    {
+        // TODO: trigger door sound
+        doorOpen.SetActive(true);
+        doorClosed.SetActive(false);
+        boxCollider.enabled = false;
+    }
+
+    public void CloseDoor()
+    {
+        // TODO: trigger door sound
+        doorOpen.SetActive(false);
+        doorClosed.SetActive(true);
+        boxCollider.enabled = true;
+    }
+}
